57c29c3e5150bd9ab74dac70e3a1a0f36810afe2
serheihimself <sergm080@gmail.com>
2022-12-13 23:39:53 +0200

    task-1_commit

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,7 @@
-const listEl = document.getElementById("categories").children.length
-console.log("Number of categories:", listEl)
+const itemList = document.querySelectorAll('.item');
+console.log(`Number of categories: ${itemList.length}`)
 
-const itemEl = document.querySelectorAll(".item");
-const packEl = itemEl[0];
-console.log("Category:", packEl.firstElementChild.textContent)
-console.log("Elements:", packEl.lastElementChild.children.length)
-
-const boxEl = itemEl[1];
-console.log("Category:", boxEl.firstElementChild.textContent)
-console.log("Elements:", boxEl.lastElementChild.children.length)
-
-const caseEl = itemEl[2];
-console.log("Category:", caseEl.firstElementChild.textContent)
-console.log("Elements:", caseEl.lastElementChild.children.length);
+itemList.forEach(item => {
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements: ${item.lastElementChild.children.length}`)
+});
